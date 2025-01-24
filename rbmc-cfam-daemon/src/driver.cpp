4ff9ea2efed9bf69b5f462a312cbb02f4feadf35
Matt Spinler <spinler@us.ibm.com>
2025-01-24 13:00:50 -0600

    CFAM: Rename SysFS class to Driver
    In preparation for a change to the FSI device driver coming that removes
    the scratchpad register sysfs files in favor of using ioctl(), rename
    the SysFS class to the Driver class.  There are no functional changes.
    
    Change-Id: Ibdb7367e8267bd65dd3985b8f3684d9f870ef395
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- rbmc-cfam-daemon/src/driver.cpp
+++ rbmc-cfam-daemon/src/driver.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-#include "sysfs.hpp"
+#include "driver.hpp"
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -25,7 +25,7 @@ struct Closer
 };
 
 std::expected<uint32_t, int>
-    SysFSImpl::read(const std::filesystem::path& file) const
+    DriverImpl::read(const std::filesystem::path& file) const
 {
     try
     {
@@ -74,7 +74,7 @@ std::expected<uint32_t, int>
     return value;
 }
 
-int SysFSImpl::write(const std::filesystem::path& file, uint32_t value) const
+int DriverImpl::write(const std::filesystem::path& file, uint32_t value) const
 {
     try
     {
